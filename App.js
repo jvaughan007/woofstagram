2cc7ee050655d3b2158f922eddda7eda6b4527b3
Joshua Vaughan <josh.vaughan@instantteams.com>
2024-01-09 16:49:50 -0600

    added formik, ToDo: adjust autoCapitalize

--- App.js
+++ App.js
@@ -1,28 +1,14 @@
-import React, { useState } from 'react';
-import { TextInput, Text, View, ScrollView } from 'react-native';
+import React from 'react';
+import { TextInput, Text, View, ScrollView, Button } from 'react-native';
 import { Formik } from 'formik';
 
 const App = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPass, setConfirmPass] = useState('');
-  const [name, setName] = useState('');
-  const [dob, setDob] = useState('');
-  const [breed, setBreed] = useState('');
-  const [toy, setToy] = useState('');
-
-  const confirmPasswordsMatch = (props) => {
-    const { nativeEvent: { text } } = props;
-
-    if (text !== password) {
-      alert('Passwords do not match, please try again.')
-    }
-
-  }
 
   const onSubmit = (values) => {
     if (values.password !== values.confirmPass) {
       alert('Passwords do not match, please try again.');
+    } else {
+      console.log('Yuhhhhhhhh');
     }
   }
 
@@ -40,64 +26,62 @@ const App = () => {
       }}
       onSubmit={onSubmit}
     >
-      
       {({ handleChange, handleSubmit, values }) => (
-          <ScrollView
-          >
-            <InputWithLabel
-              label='Email'
-              placeholder='Type your email here'
-              value={values.email}
-              onChangeText={handleChange('email')}
-            />
-            <InputWithLabel
-              label='Password'
-              placeholder='Type your password here'
-              value={values.password}
-              onChangeText={handleChange('password')}
-            />
-            <InputWithLabel
-              label='Confirm Password'
-              placeholder='Confirm Password Here'
-              value={values.confirmPass}
-              onChangeText={handleChange('confirmPass')}
-              onSubmitEditing={handleSubmit}
-            />
-            <InputWithLabel
-              label='Name'
-              placeholder="Type your pet's name here"
-              value={name}
-              onChangeText={setName}
-            />
-            <InputWithLabel
-              label='Date of Birth'
-              placeholder="Type your pet's birthday here"
-              value={dob}
-              onChangeText={setDob}
-            />
-            <InputWithLabel
-              label='Breed'
-              placeholder="Type your pet's breed here"
-              value={breed}
-              onChangeText={setBreed}
-            />
-            <InputWithLabel
-              label='Toy'
-              placeholder="Type your pet's toy here"
-              value={toy}
-              onChangeText={setToy}
-            />
-          </ScrollView>
-        )
-      }
-      
+        <ScrollView
+        >
+          <InputWithLabel
+            label='Email'
+            placeholder='Type your email here'
+            value={values.email}
+            onChangeText={handleChange('email')}
+          />
+          <InputWithLabel
+            label='Password'
+            placeholder='Type your password here'
+            value={values.password}
+            onChangeText={handleChange('password')}
+          />
+          <InputWithLabel
+            label='Confirm Password'
+            placeholder='Confirm Password Here'
+            value={values.confirmPass}
+            onChangeText={handleChange('confirmPass')}
+          />
+          <InputWithLabel
+            label='Name'
+            placeholder="Type your pet's name here"
+            value={values.name}
+            onChangeText={handleChange('name')}
+          />
+          <InputWithLabel
+            label='Date of Birth'
+            placeholder="Type your pet's birthday here"
+            value={values.dob}
+            onChangeText={handleChange('dob')}
+          />
+          <InputWithLabel
+            label='Breed'
+            placeholder="Type your pet's breed here"
+            value={values.breed}
+            onChangeText={handleChange('breed')}
+          />
+          <InputWithLabel
+            label='Toy'
+            placeholder="Type your pet's toy here"
+            value={values.toy}
+            onChangeText={handleChange('toy')}
+          />
+          <Button onPress={handleSubmit} title="Submit" />
+        </ScrollView>
+      )}
+    
     </Formik>
   );
 
 };
 
 const InputWithLabel = (props) => {
-  const { label, placeholder, value, onChangeText, onSubmitEditing } = props;
+  const { label, placeholder, value, onChangeText, onSubmitEditing,  } = props;
 
   return (
     <View style={{ padding: 32 }}>
@@ -110,7 +94,7 @@ const InputWithLabel = (props) => {
         onChangeText={onChangeText}
         secureTextEntry={label == 'Password' || label == 'Confirm Password' ? true : false}
         onSubmitEditing={onSubmitEditing}
-        style={styles.inputClass}
+        style={{ padding: 8, fontSize: 16 }}
       />
     </View>
   );
---
+++ first-app/first-app/first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_appApp.swift
+//  first-app
+//
+//  Created by JoshCodesIRL on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
